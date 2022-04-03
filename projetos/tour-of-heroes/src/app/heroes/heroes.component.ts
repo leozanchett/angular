9195d6c453b6af9d02442e96d6d62240eda2d9f6
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-03 16:38:47 -0300

    POST method

--- projetos/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ projetos/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -25,11 +25,21 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroes = this.heroService.getHeroes();    
   }*/
-  
+
   // Observable
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (name) {
+      this.heroService.addHero({ name } as Hero)
+        .subscribe(hero => {
+          this.heroes.push(hero);
+        });
+    }
   }
 
 }
