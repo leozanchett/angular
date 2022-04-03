c193d9772dbb8da751b31e8f136ca9093a098dd2
Leo <45577227+leozanchett@users.noreply.github.com>
2022-04-02 22:07:38 -0300

    Hero Tour Project

--- projetos/tour-of-heroes/src/app/app.module.ts
+++ projetos/tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ControllerComponent } from './controller/controller/controller.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ControllerComponent
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
